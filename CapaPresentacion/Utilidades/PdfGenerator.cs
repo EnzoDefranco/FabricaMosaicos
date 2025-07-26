6dc18558ec802ecb23354780a957033d8568fb05
Enzo Defranco <enzolobod22@email.com>
2025-07-25 22:08:04 -0300

    ok

---
+++ CapaPresentacion/Utilidades/PdfGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DinkToPdf;
+
+namespace CapaPresentacion.Utilidades
+{
+    internal class PdfGenerator
+    {
+        private static readonly SynchronizedConverter _converter = new SynchronizedConverter(new PdfTools());
+
+        public static void Generar(string html, string rutaSalida)
+        {
+            var doc = new HtmlToPdfDocument
+            {
+                GlobalSettings = new GlobalSettings
+                {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.A4,
+                    Margins = new MarginSettings { Top = 10, Bottom = 10 },
+                    Out = rutaSalida
+                },
+                Objects = {
+                    new ObjectSettings
+                    {
+                        HtmlContent = html,
+                        WebSettings = new WebSettings
+                        {
+                            DefaultEncoding = "utf-8",
+                            LoadImages = true
+                        }
+                    }
+                }
+            };
+
+            _converter.Convert(doc);
+        }
+    }
+}
